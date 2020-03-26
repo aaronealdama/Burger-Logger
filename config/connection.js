c25f59cdd0019474a5bbb5289ee1445273b25aba
Aaron Ealdama <aaronealdama@gmail.com>
2020-03-26 00:48:17 -0500

    im getting desperate

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "glacial-earth-57854.herokuapp.com",
   port: 3306,
   user: "root",
   password: "Acegunner145",
